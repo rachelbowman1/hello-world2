96cdb9fedd6c76b3f8fc22d3db96996724596d8f
Rachel Bowman <rachel.bowman1@pcc.edu>
2016-10-23 10:18:41 -0700

    add printed date

--- src/pcc/cis/helloworld2/HelloWorld.java
+++ src/pcc/cis/helloworld2/HelloWorld.java
@@ -7,5 +7,6 @@ import java.util.Date;
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new Date());
     }
 }

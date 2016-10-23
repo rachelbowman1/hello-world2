bdbe30c5b1d06787d1899a371e0545c1fbce775a
Rachel Bowman <rachel.bowman1@pcc.edu>
2016-10-23 10:15:35 -0700

    initial commit

---
+++ src/pcc/cis/helloworld2/HelloWorld.java
@@ -0,0 +1,11 @@
+package pcc.cis.helloworld2;
+import java.util.Date;
+
+/**
+ * Created by Rachel Bowman on 10/23/16.
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+    }
+}
